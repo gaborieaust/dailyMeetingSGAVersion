6dfd1e1157ab47bd2d70f01ae84245cd903838c6
Michael <eoloneox@gmail.com>
2021-06-10 17:27:39 +0200

    model done

--- src/main/java/com/example/dailymeeting/model/Participation.java
+++ src/main/java/com/example/dailymeeting/model/Participation.java
@@ -1,4 +1,51 @@
 package com.example.dailymeeting.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.sql.Timestamp;
+
+@Entity
+@IdClass(ParticipationId.class)
 public class Participation {
+    @Id
+    private Long userIdx;
+    @Id
+    private Long meetingIdx;
+    private Timestamp speakingDuration;
+
+    public Participation(Long userIdx, Long meetingIdx, Timestamp speakingDuration) {
+        this.userIdx = userIdx;
+        this.meetingIdx = meetingIdx;
+        this.speakingDuration = speakingDuration;
+    }
+
+    public Participation(){
+    }
+
+    public Long getUserIdx() {
+        return userIdx;
+    }
+
+    public void setUserIdx(Long userIdx) {
+        this.userIdx = userIdx;
+    }
+
+    public Long getMeetingIdx() {
+        return meetingIdx;
+    }
+
+    public void setMeetingIdx(Long meetingIdx) {
+        this.meetingIdx = meetingIdx;
+    }
+
+    public Timestamp getSpeakingDuration() {
+        return speakingDuration;
+    }
+
+    public void setSpeakingDuration(Timestamp speakingDuration) {
+        this.speakingDuration = speakingDuration;
+    }
+
+
 }
---
+++ src/main/java/com/example/dailymeeting/model/ParticipationId.java
@@ -0,0 +1,49 @@
+package com.example.dailymeeting.model;
+
+import org.springframework.core.serializer.Serializer;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class ParticipationId implements Serializable {
+    private Long userIdx;
+    private Long meetingIdx;
+
+    public ParticipationId(Long userIdx, Long meetingIdx) {
+        this.userIdx = userIdx;
+        this.meetingIdx = meetingIdx;
+    }
+
+    public Long getUserIdx() {
+        return userIdx;
+    }
+
+    public void setUserIdx(Long userIdx) {
+        this.userIdx = userIdx;
+    }
+
+    public Long getMeetingIdx() {
+        return meetingIdx;
+    }
+
+    public void setMeetingIdx(Long meetingIdx) {
+        this.meetingIdx = meetingIdx;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParticipationId that = (ParticipationId) o;
+        return Objects.equals(userIdx, that.userIdx) && Objects.equals(meetingIdx, that.meetingIdx);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userIdx, meetingIdx);
+    }
+
+    public ParticipationId() {
+    }
+   }
+
