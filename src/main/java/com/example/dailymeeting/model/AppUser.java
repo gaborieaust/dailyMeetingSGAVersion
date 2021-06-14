851bd14a9876237d38ebc86f785ab8cb7ddcb348
Steven Gaborieau <gaborieau.s@hotmail.fr>
2021-06-14 14:32:49 +0200

    @Jsonignore

--- src/main/java/com/example/dailymeeting/model/AppUser.java
+++ src/main/java/com/example/dailymeeting/model/AppUser.java
@@ -1,5 +1,7 @@
 package com.example.dailymeeting.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -11,6 +13,7 @@ public class AppUser {
     @SequenceGenerator(name = "user_id_seq", allocationSize = 1)
     private Long id;
 
+    @JsonIgnore
     @OneToMany
     private List<Participation> participationList;
     private String name;
