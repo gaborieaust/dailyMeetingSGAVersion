48d90161c1dc5a47526ae000cdec48e68816848b
Michael <eoloneox@gmail.com>
2021-07-13 10:50:16 +0200

    add coponnt authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule
