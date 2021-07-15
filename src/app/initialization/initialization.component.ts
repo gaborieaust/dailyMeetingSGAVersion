b6954bb141b5f25d6ed7a9fec60594a36589b8d9
Steven Gaborieau <gaborieau.s@hotmail.fr>
2021-07-15 15:46:14 +0200

    fonction initializtion ok

--- src/app/initialization/initialization.component.ts
+++ src/app/initialization/initialization.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Service} from "../service";
 import {formatDate} from "@angular/common";
 import {createComponent} from "@angular/compiler/src/core";
@@ -10,7 +10,8 @@ import {createComponent} from "@angular/compiler/src/core";
 })
 export class InitializationComponent implements OnInit {
 
-  constructor(private service: Service,) { }
+  constructor(private service: Service,) {
+  }
 
   // présente le boutton permettant d'initaliser le meeting
   // au clic: vérifie en bdd la présence ou non d'un meeting pour la date du jour
@@ -18,35 +19,29 @@ export class InitializationComponent implements OnInit {
   // si non => crée le meeting en bdd et lance la fonction d'initialisation de la usersMeetingList
 
   ngOnInit(): void {
+    // to test if the meeting has been created
+    this.service.getLastMeeting().subscribe(lastMeeting => {
+        let lastMeetingDate: string = new Date(lastMeeting.date).toISOString().substring(0, 10);
+        console.log(lastMeetingDate)
+        // comparison date of the day with date of the last meeting
+        let today = new Date();
+        let dateOfTheDay = today.getFullYear() + '-' + ("0" + (today.getMonth() + 1)).slice(-2) + '-' + today.getDate()
+        console.log(dateOfTheDay)
+        if (lastMeetingDate == dateOfTheDay) {
+          location.assign('http://localhost:4200/meeting/')
+        }
+      }
+    )
   }
 
-  initMeeting(){
-  this.service.getLastMeeting().subscribe(lastMeeting => {
-    let lastMeetingDate: string = new Date(lastMeeting.date).toISOString().substring(0, 10);
-    console.log(lastMeetingDate)
-    // comparison date of the day with date of the last meeting
-    let today = new Date();
-    let dateOfTheDay = today.getFullYear()+'-'+("0" + (today.getMonth() + 1)).slice(-2)+'-'+today.getDate()
-    console.log(dateOfTheDay)
-    //si oui => route vers user-list-component
-    if (lastMeetingDate==dateOfTheDay){
-      // creation du meeting
-      // @ts-ignore
-      this.service.createMeeting({
-        "id": 4,
-        "date": '2019-06-11'
+  initMeeting() {
+    //todo régler l'issue lorsque deux utilisateurs cliquent en même temps sur la fonction de création d'un meeting;
 
-        //"date": new Date(2021)
-      })
-      ()
-      console.log("elle est bonne la date")}
-
-    //{location.assign('http://localhost:4200/meeting/')}
-
-
-    //
-
-  })
-
+    // @ts-ignore
+    this.service.createMeeting({
+      "date": new Date(Date.now())
+    }).subscribe()
+    location.assign('http://localhost:4200/meeting/')
   }
+
 }
