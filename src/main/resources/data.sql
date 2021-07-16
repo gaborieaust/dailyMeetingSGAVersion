2d5d2cf599cf67d8594dab3e7e7d0ef820c8492e
David Wissocq <dwissocq@gmail.com>
2021-07-16 11:27:29 +0200

    fix istimekeeper

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,8 @@ insert into appuser (id, isactive,name) values (5,true, 'Guy G');
 INSERT INTO meeting (id, date) VALUES (1, '2021-07-15 10:34:16.000000');
 INSERT INTO meeting (id, date) VALUES (2, '2021-06-09 10:34:27.000000');
 INSERT INTO meeting (id, date) VALUES (3, '2021-06-04 10:34:40.000000');
-INSERT INTO participation (id, speakingduration, appuser_id, meeting_id) VALUES (1, 70, 1, 1);
-INSERT INTO participation (id, speakingduration, appuser_id, meeting_id) VALUES (2, 110, 2, 1);
-INSERT INTO participation (id, speakingduration, appuser_id, meeting_id) VALUES (3, 54, 1, 2);
-INSERT INTO participation (id, speakingduration, appuser_id, meeting_id) VALUES (4, 12, 3, 2);
+INSERT INTO participation (id, speakingduration, appuser_id, meeting_id, istimekeeper) VALUES (1, 70, 1, 1, false);
+INSERT INTO participation (id, speakingduration, appuser_id, meeting_id, istimekeeper) VALUES (2, 110, 2, 1, false);
+INSERT INTO participation (id, speakingduration, appuser_id, meeting_id, istimekeeper) VALUES (3, 54, 1, 2, false);
+INSERT INTO participation (id, speakingduration, appuser_id, meeting_id, istimekeeper) VALUES (4, 12, 3, 2, false);
 
