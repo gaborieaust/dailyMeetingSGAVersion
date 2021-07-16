8bfa10e01b6528fd83ecef1b8ec9e39019900d0b
Michael <eoloneox@gmail.com>
2021-07-16 10:55:18 +0200

    Fix CORS issue

--- src/main/java/com/example/dailymeeting/DailyMeetingApplication.java
+++ src/main/java/com/example/dailymeeting/DailyMeetingApplication.java
@@ -6,6 +6,9 @@ import com.example.dailymeeting.service.StartMeeting;
 import org.aspectj.weaver.ast.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
@@ -13,9 +16,20 @@ import java.util.List;
 
 @EnableSwagger2
 @SpringBootApplication
+
 public class DailyMeetingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DailyMeetingApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
