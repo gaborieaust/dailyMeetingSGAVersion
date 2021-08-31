b6d0697a3e69d73cb2c402c5581cbaa962af0cd3
Steven Gaborieau <gaborieau.s@hotmail.fr>
2021-08-31 12:49:46 +0200

    share data btw componants

--- src/app/meeting-board/meeting-board.component.ts
+++ src/app/meeting-board/meeting-board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersListService} from "../users-list.service";
+import {AppUserMeeting} from "../appUser";
 
 @Component({
   selector: 'app-meeting-board',
@@ -7,21 +8,50 @@ import {UsersListService} from "../users-list.service";
   styleUrls: ['./meeting-board.component.css']
 })
 export class MeetingBoardComponent implements OnInit {
-  meetingStarted = false;
+  //lastUser = false ;
+  //meetingStarted = false;
+  currentSpeaker : AppUserMeeting = {
+    id: 0,
+    name: "test",
+    isParticipant: true,
+    isSpeaking: true,
+    timeKeeper: true,
+      } ;
+  //index : number =0;
   constructor(
-    private usersListService : UsersListService
+    public usersListService : UsersListService
   ) { }
 
   ngOnInit(): void {
   }
 
   startMeeting () {
-    this.meetingStarted = true;
+    this.usersListService.meetingStarted = true;
+    // boucler sur la liste des appUserMeetingList et mettre dans la boucle un push si le user est participant sinon le retirer et le mettre dans une autre
+    for ( let appUserMeeting  in this.usersListService.appUsersMeetingList) {
+        console.log(appUserMeeting)
+    }
     //générer la liste aléatoire de participants
+
     this.usersListService.usersListRandomlySorted();
-    //location.assign('http://localhost:4200/meeting/')
     //remise en ordre suivant la liste dans le composant app users list
     // afficher le premier participant dans le componant meeting board
+    this.currentSpeaker = this.usersListService.appUsersMeetingList[this.usersListService.index];
     //faire apparaître les boutons Break/next/end meeting
   }
+
+  nextSpeaker () {
+    if (this.usersListService.index == this.usersListService.appUsersMeetingList.length-1){
+      this.usersListService.lastUser = true;
+    } else {
+      this.usersListService.index = this.usersListService.index + 1;
+      this.currentSpeaker = this.usersListService.appUsersMeetingList[this.usersListService.index];
+    }
+    //sessionStorage.setItem(this.usersListService.index);
+    //if (this.usersListService.index == this.usersListService.appUsersMeetingList.length){
+
+  }
+
+
+
 }
--- src/app/users-list.service.ts
+++ src/app/users-list.service.ts
@@ -6,10 +6,24 @@ import {AppUserMeeting} from "./appUser";
 })
 export class UsersListService {
   appUsersMeetingList: AppUserMeeting[] = [];
+  appUsersMeetingListIsPart : AppUserMeeting[] = [];
+  meetingStarted = false;
+  index : number =0;
+  lastUser = false ;
+
+
+
   constructor() {
   }
   usersListRandomlySorted(){
     this.appUsersMeetingList.sort(()=> Math.random() - 0.5);
   }
+  splitIntoParticipantNot(){
+    // while et slice pour injecter la liste des participants dans la liste des appUsersMeetingList
+    for ( let appUserMeeting  : this.appUsersMeetingList){
+
+    }
+  }
+
 
 }
