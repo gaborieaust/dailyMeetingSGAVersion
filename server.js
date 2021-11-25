ae898ce8f7a9102fad3db141a8633ea4edec682f
Steven Gaborieau <gaborieau.s@hotmail.fr>
2021-11-25 13:58:21 +0100

    modifDeploy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("./dist/daily-meeting-angular"));
+app.use(express.static(__dirname +'./dist/daily-meeting-angular'));
 
 app.get("/*", function (req, res) {
-  res.sendFile(res.sendFile(path.join("./dist/daily-meeting-angular", 'index.html'));;
+  res.sendFile(path.join(__dirname+'/dist/daily-meeting-angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
