f6200a07367ab5440036088a4cd663a388aef488
Steven Gaborieau <stevengaborieau@appleusers-MacBook-Air.local>
2021-12-08 19:12:37 -0500

    deployHeroku

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-
-app.use(express.static(path.join(__dirname,'../dist/daily-meeting-angular/')));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname+'/dist/daily-meeting-angular/index.html'));
-});
-
+app.use(express.static(__dirname + '/dist/daily-meeting-angular'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/daily-meeting-angular/index.html'));});
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`);
+console.log ('je suis ici')
+console.log(__dirname)
