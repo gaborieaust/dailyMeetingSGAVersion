eb44cfd9fe68f160bdda35a41cbd18ebf04c0c8e
Steven Gaborieau <stevengaborieau@appleusers-MacBook-Air.local>
2021-12-09 01:35:10 -0500

    cert server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.use(express.static(__dirname + '/dist/daily-meeting-angular'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/daily-meeting-angular/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
 console.log ('je suis ici')
 console.log(__dirname)
