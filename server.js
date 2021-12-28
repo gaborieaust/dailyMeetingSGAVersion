073721aa87e75d6194d1bd1306f32f97524448b3
Steven Gaborieau <stevengaborieau@appleusers-air.home>
2021-12-28 15:35:48 +0100

    TryStaticJson

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ app.use(express.static(__dirname + '/dist/daily-meeting-angular'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
       '/dist/daily-meeting-angular/index.html'));});
-app.get('/meeting', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/daily-meeting-angular/index.html'));});
 
 
 
