cec44c00188f2a0e2bdc38c23631de76ba412137
Steven Gaborieau <stevengaborieau@appleusers-MacBook-Air.local>
2021-12-28 20:48:52 +0100

    moveDependencies

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const meetingRouter = require('./meeting');
 
 const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/daily-meeting-angular'));
-app.use('/meeting', meetingRouter);
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
       '/dist/daily-meeting-angular/index.html'));});
