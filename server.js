90f8c2847933f77391d998094206886aebce1fd3
Steven Gaborieau <stevengaborieau@appleusers-MacBook-Air.local>
2021-12-28 21:03:46 +0100

    test

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/daily-meeting-angular'));
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname+
       '/dist/daily-meeting-angular/index.html'));});
 
-
+app.all('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/dist/daily-meeting-angular/index.html');
+});
 app.listen(process.env.PORT || 8080);
 
 console.log ('je suis ici')
